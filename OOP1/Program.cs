a9535a0705dfb1f8bc7e94e4d87be79412879525
Ayşegül Ayvaz <152120191068@ogrenci.ogu.edu.tr>
2021-02-05 01:57:36 +0300

    20 Ocak

---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryID = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+            Product product2 = new Product() { ID = 2, CategoryID = 3, ProductName = "sandalye", UnitPrice = 250, UnitInStock = 6 };
+
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            
+           
+           
+
+
+
+
+        }
+    }
+}
