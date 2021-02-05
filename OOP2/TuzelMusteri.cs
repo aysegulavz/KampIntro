ac9c567e634c6f768abb3ca45728b7d1dc1768ca
Ayşegül Ayvaz <152120191068@ogrenci.ogu.edu.tr>
2021-02-06 00:54:29 +0300

    23 Ocak

---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //coorporate
+    class TuzelMusteri:Musteri //inheritance-miras
+    {
+        
+        public string VergiNo { get; set; }
+        public string SirketAdi { get; set; }
+
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtıyacKrediManager ıhtıyacKrediManager = new IhtıyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseloggerservice = new DatabseLoggerService();
+            ILoggerService fileloggerservice = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager,databaseloggerservice);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtıyacKrediManager,tasitKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            
+            
+        }
+    }
+}
